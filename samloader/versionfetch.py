2c4139ef9ebe67b774a935d8d5cb7384a8322bc5
Anan Jaser <ananjaser@gmail.com>
2023-10-08 23:25:40 +0400

    samloader: versionfetch: attach user-agent
    * Fixes 400 Client Error: Bad Request

--- samloader/versionfetch.py
+++ samloader/versionfetch.py
@@ -18,7 +18,7 @@ def normalizevercode(vercode: str) -> str:
 def getlatestver(model: str, region: str) -> str:
     """ Get the latest firmware version code for a model and region. """
     req = requests.get("https://fota-cloud-dn.ospserver.net/firmware/" \
-        + region + "/" + model + "/version.xml")
+        + region + "/" + model + "/version.xml" , headers={"User-Agent": "Kies2.0_FUS"})
     if req.status_code == 403:
         raise Exception("Model or region not found (403)")
     req.raise_for_status()
